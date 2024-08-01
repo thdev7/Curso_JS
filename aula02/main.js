d77bc47c3f7f1777f57c360383548a048e3aad2d
Thiago Andrade <thiagoadd14@gmail.com>
2024-07-31 21:33:35 -0300

    aula 2

---
+++ aula02/main.js
@@ -0,0 +1,75 @@
+/*
+STRINGS
+
+const = variavel não alteravel
+let =variavel alteravel
+
+CONSOLE.LOG =  Imprime o texto no Console como uma mensagem de log. EX  
+
+const firstname = 'Thiago'
+const lastname = 'Andrade'
+
+console.log(`Meu nome é ${firstname} ${lastname}`)
+
+$= serve para usar uma variavel
+
+console.log(`Meu nome é ${firstname.toUpperCase()} ${lastname.LowerCase()}`)
+
+toUpperCase() para maiusculo   LowerCase() para minusculo
+
+
+LISTAS
+
+console.log(names.split(",")) = usado para criar uma lista de coisas declarada em uma variavel EX=
+
+const names = "Thiago,Iasmim,Dani"  (virgula é usada para separar os itens)
+
+
+
+
+NUMBERS/ INT
+
+const number = 5;
+
+CALCULOS
+
+console.log(number + 10);
+
+CONVERTENDO NUMERO PARA STRING
+
+console.log(number.toString());
+
+CHECAR TIPO DE VARIAVEL = console.log(typeof number) {int}    console.log(typeof number.toString()); {str}
+
+
+
+
+BOOLEANOS
+
+FALSE
+TRUE
+
+console.log(2 == 2) true
+console.log(2 == 3) false
+
+
+
+
+NULL E UNDEFINED
+
+const x = null; = sem valor, vazio, sem definição
+
+const y = undefined; = variavel indefinida
+
+
+
+
+LISTA e OBJETOS
+
+const list = [1,2,3,4,5];
+const object = {name: 'Thiago'} guarda propiedades e valores
+
+*/
+
+
+const message = 'Hello World' 
